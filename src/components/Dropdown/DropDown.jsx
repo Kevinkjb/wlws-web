dbc40893fb4a288b1264e51ed0a6941453904a33
Kevin <tomfge09@yahoo.ca>
2025-01-21 18:55:38 -0700

    Initial files has been updated

--- src/components/Dropdown/DropDown.jsx
+++ src/components/Dropdown/DropDown.jsx
@@ -6,8 +6,8 @@ const DropDown = () => {
   return (
     <DropdownButton id="dropdown-basic-button" title="Join Us" className="toggle"  variant="none">
       <Dropdown.Item className='drop-item' href="https://forms.gle/NZhWKrvfrpgn4mEw8" target='_blank'>Vendor</Dropdown.Item>
-      <Dropdown.Item className='drop-item' href="#">Performer</Dropdown.Item>
-      <Dropdown.Item className='drop-item' href="#">Volunteer</Dropdown.Item>
+      <Dropdown.Item className='drop-item' href="https://docs.google.com/forms/d/1FA9XqGmd9yrio1TyQO_z5KjqZ2Y3hd6_ihfj9_7ejNI/edit" target='_blank'>Performer</Dropdown.Item>
+      <Dropdown.Item className='drop-item' href="https://docs.google.com/forms/d/1_p4GXf72x5tPzPy5Pc6GHwrTyqEvvqyhyEfD5oGnOxM/edit" target='_blank'>Volunteer</Dropdown.Item>
     </DropdownButton>
   )
 }

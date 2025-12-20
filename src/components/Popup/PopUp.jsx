1b76fa3a0e1635121873b872ea2659ab061b15dd
Kevin <tomfge09@yahoo.ca>
2025-12-20 15:12:47 -0700

    Initial files has been updated

--- src/components/Popup/PopUp.jsx
+++ src/components/Popup/PopUp.jsx
@@ -2,7 +2,7 @@
 import { FaRegWindowClose } from "react-icons/fa";
 import './popup.css'
 
-import popImg from '../../images/advertisement/heritage.png'
+import popImg from '../../images/advertisement/advertisement.jpg'
 const Popup = (props) => {
   const handleImageClick = () => {
     props.setTrigger(false);  // Close the popup when the image is clicked
--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -37,7 +37,7 @@ const HomePage = () => {
 
     const timer = setTimeout(() => {
       setTrigger(false);  
-    }, 5000); 
+    }, 10000); 
 
     return () => clearTimeout(timer);  
   }, []); 

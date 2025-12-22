840ac7bcea85c293bbc7a0bbda35cbfc414eaf66
Kevin <tomfge09@yahoo.ca>
2025-12-22 15:57:43 -0700

    Initial files has been updated

--- src/components/Dropdown/DropDown.jsx
+++ src/components/Dropdown/DropDown.jsx
@@ -5,9 +5,9 @@ import './dropdown.css'
 const DropDown = () => {
   return (
     <DropdownButton id="dropdown-basic-button" title="Join Us" className="toggle"  variant="none">
-      <Dropdown.Item className='drop-item' href="https://forms.gle/NZhWKrvfrpgn4mEw8" target='_blank'>Vendor</Dropdown.Item>
-      <Dropdown.Item className='drop-item' href="https://docs.google.com/forms/d/1FA9XqGmd9yrio1TyQO_z5KjqZ2Y3hd6_ihfj9_7ejNI/edit" target='_blank'>Performer</Dropdown.Item>
-      <Dropdown.Item className='drop-item' href="https://docs.google.com/forms/d/1_p4GXf72x5tPzPy5Pc6GHwrTyqEvvqyhyEfD5oGnOxM/edit" target='_blank'>Volunteer</Dropdown.Item>
+      <Dropdown.Item className='drop-item' href="https://docs.google.com/forms/d/1cxCfEqPI89PQw_37ElrmwRO9n35dPSbl5gHeFB16Uv8/viewform?pli=1&pli=1&edit_requested=true" target='_blank'>Vendor</Dropdown.Item>
+      <Dropdown.Item className='drop-item' href="https://docs.google.com/forms/d/1aXYwv0YNNxhrl0yaJng2cAevui57YY13iZoIrJ-47Ew/viewform?edit_requested=true" target='_blank'>Performer</Dropdown.Item>
+      <Dropdown.Item className='drop-item' href="https://docs.google.com/forms/d/105tZQJHAtGt1Dx1721b8_kQTiBnfzXbJ6ZgpKwbhhr8/viewform?edit_requested=true" target='_blank'>Volunteer</Dropdown.Item>
     </DropdownButton>
   )
 }
